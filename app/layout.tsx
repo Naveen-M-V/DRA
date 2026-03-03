d17788a0a611b1d16c360904bdbee14c2ffa3ad0
Naveen MV <mvnaveen18@gmail.com>
2026-03-03 21:00:54 +0530

    Meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,32 @@ export default function RootLayout({
           src="https://forms.cdn.sell.do/t/5ba883447c0dac3321d9f483.js"
           strategy="afterInteractive"
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1852333405356715');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1852333405356715&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
